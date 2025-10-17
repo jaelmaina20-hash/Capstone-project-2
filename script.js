1094511c70be456f28ca33e51043dc3641c60dc6
jaelmaina20-hash <jaelmaina2.0@gmail.com>
2025-10-18 01:07:42 +0300

    hero section and javascript

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const mobileMenu = document.getElementById('mobile-menu');
 hamburgerMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
 });
+
+const images = [Image1, Image2, Image3];
+  const captions = [caption1, caption2, caption3];
+  let current = 0;
+  const total = images.length;
+
+  setInterval(() => {
+    images[current].style.opacity = 0;
+    captions[current].style.opacity = 0;
+    current = (current + 1) % total;
+    images[current].style.opacity = 1;
+    captions[current].style.opacity = 1;
+  }, 6000);
